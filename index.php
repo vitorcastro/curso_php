a06b1c60d4e066eb8abd063372d948b987ed64af
Vitor Castro <castro.vitor006@gmail.com>
2013-05-19 17:35:46 -0300

    Iniciando o PHP - aula 1

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
-<?php
+<h1>TODO List</h1>
 
-echo 'Hello Word';
+<br>
+
+<?php
+// php do ser "aberto" <? 
+
+/**
+ * Coment‡rio
+ * v‡rias
+ * linhas 
+ */
+// Coment‡rio uma linha
+
+// comando echo Ž mais r‡pido que o comando print
+echo 'Hello Word '; 
+
+// uso o ponto (.) para concatencao
+// echo, uso virgula para escreve instru›es  
+
+print 'Hello Word';
+
+$aluno = 'Vitor Castro';
+
+echo $aluno , 'éŽ o instrutor';
+
+
+
 
 
 ?>
+
+<h2>TESTE</h2>
